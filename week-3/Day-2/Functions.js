c6c26779a4e6b42823f07959798c064b07909b56
lakshmi narasimha <mlakshminarasimh95a@gmail.com>
2024-07-15 13:05:42 +0530

    intial commit

---
+++ week-3/Day-2/Functions.js
@@ -0,0 +1,5 @@
+k = function name(a,b){
+    return (a+b);
+}
+h = k(34,18)
+console.log(h);
---
+++ week-3/Day-2/variables.js
@@ -0,0 +1,11 @@
+// 8 Types of Datatypes
+// Numbers
+// float
+let h = 6;
+{
+    let a = 3784;
+    const b = 7;
+    var c = 67;
+    d = 78;
+}
+console.log(h);
